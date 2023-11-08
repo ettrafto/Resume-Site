ea451dbe53f9c3a86f954d54dbd4466fa8010189
Evan Trafton <Evan.Trafton@uvm.edu>
2023-11-08 13:26:56 -0500

    building framework from database design

---
+++ erd.php
@@ -0,0 +1,13 @@
+<?php
+include 'top.php';
+?>
+
+    <h2>Lab 6: ERD</h2>
+
+    <figure>
+        <img style="max-width:100%" alt="Quiz ERD" src="images/ERD.jpg">
+    </figure>
+<?php include 'footer.php';?>    
+
+    </body>
+</html>
---
+++ top.php
@@ -0,0 +1,34 @@
+<?php 
+$phpSelf=htmlspecialchars($_SERVER['PHP_SELF']);
+$pathParts = pathinfo($phpSelf);
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>CS2480 Lab6</title>
+        <meta name="author" content="Evan Trafton">
+        <meta name="description" content="CS2480 Lab6 ">
+        
+        <meta name="viewport" content="width=device-width, intial-scale=1.0">
+        
+        <!--<link rel="favicon icon" href="images/assets/favicon.ico" type="image/x-icon">-->
+        
+        <link rel="stylesheet" href="css/custom.css?version=<?php print time(); ?>" type="text/css">
+        <link rel="stylesheet" media="(max-width: 800px)" href="css/tablet.css?version=<?php print time(); ?>" type="text/css">
+        <link rel="stylesheet" media="(max-width: 600px)" href="css/phone.css?version=<?php print time(); ?>" type="text/css">
+
+        <!-- Javascript link would go here  -->
+    </head>
+
+
+    <?php
+    print '<body>' . PHP_EOL;
+    include 'Connect-DB.php';
+    print PHP_EOL;
+    include 'header.php';
+    print PHP_EOL;
+    include 'nav.php';
+    print PHP_EOL;
+    ?>
